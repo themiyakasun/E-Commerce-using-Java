92e7e6cbbffe3c908c7aa77903122a69eb5f5bf4
themiyakasun <themiyakasun.tb@gmail.com>
2024-04-02 23:08:13 +0530

    Button Component

---
+++ src/java/myPackage/ui/Button.java
@@ -0,0 +1,60 @@
+package myPackage.ui;
+
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.tagext.JspFragment;
+import javax.servlet.jsp.tagext.SimpleTagSupport;
+
+public class Button extends SimpleTagSupport {
+
+    private String text;
+    private boolean outlined;
+    private boolean rounded;
+
+    
+    @Override
+    public void doTag() throws JspException {
+        JspWriter out = getJspContext().getOut();
+        
+        try {
+
+            JspFragment f = getJspBody();
+            if (f != null) {
+                f.invoke(out);
+            }
+            
+            String buttonStyle = "";
+        
+            if(outlined){
+                buttonStyle = "border: 1px solid #141718; color: #141718; background-color: #fff;";
+            }else {
+                buttonStyle = "background-color: #141718; color: #fff;";
+            }
+
+            if (rounded) {
+                buttonStyle += " border-radius: 80px;";
+            }else {
+                buttonStyle += " border-radius: 8px;";
+            }
+
+            out.println(String.format("<button class=\"button\" style=\"" + buttonStyle + "\">%s</button>", text));
+
+
+        } catch (java.io.IOException ex) {
+            throw new JspException("Error in Button tag", ex);
+        }
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public void setOutlined(boolean outlined) {
+        this.outlined = outlined;
+    }
+
+    public void setRounded(boolean rounded) {
+        this.rounded = rounded;
+    }
+    
+}
