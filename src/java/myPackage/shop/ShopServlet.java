6508d41b66a82d8a02c69916fedff8f7611111c5
Kaveeshe Waduge <109468529+Kaveeshe2001@users.noreply.github.com>
2024-04-28 13:13:05 +0530

    shopservlet

--- src/java/myPackage/shop/ShopServlet.java
+++ src/java/myPackage/shop/ShopServlet.java
@@ -1,88 +1,57 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package myPackage.shop;
 
+import com.google.gson.Gson;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import myPackage.DbUtil;
 
-/**
- *
- * @author Ka Vi Ya 2001
- */
+
 @WebServlet(name = "ShopServlet", urlPatterns = {"/ShopServlet"})
 public class ShopServlet extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet ShopServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet ShopServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        
+        ArrayList<Shop> shops = getShopsFromDb();
+        String json = new Gson().toJson(shops);
+        
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        
+        response.getWriter().write(json);
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
+    private ArrayList<Shop> getShopsFromDb() {
+        ArrayList<Shop> shops = new ArrayList<>();
+        
+        try(Connection conn = DbUtil.getConnection()){
+           PreparedStatement statement = conn.prepareStatement("SELECT * FROM products");
+           try(ResultSet rs = statement.executeQuery()){
+               while(rs.next()){
+                   int proId = rs.getInt("pro_id");
+                   String proName = rs.getString("pro_name");
+                   double discountPrice = rs.getDouble("discount_price");
+                   double proPrice = rs.getDouble("pro_price");
+                   String proImg = rs.getString("pro_img");
+                   
+                   shops.add(new Shop(proId, proName, discountPrice, proPrice, proImg));
+               }
+           }
+        } catch (SQLException e) {
+            e.getMessage();
+        }
+        
+        return shops;
     }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
 }
